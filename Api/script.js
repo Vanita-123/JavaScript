62c4d271a35be20611e729e6ad3ff9f27f300dec
Vanita <vanitabarhman9728@gmail.com>
2024-09-24 13:30:52 +0530

    Api-handle

---
+++ Api/script.js
@@ -0,0 +1,28 @@
+// https://jsonplaceholder.typicode.com/users
+let Card = document.getElementById("Card");
+const fetchApi = async () => {
+  let API_URL = "https://jsonplaceholder.typicode.com/users";
+  try {
+    let dataresponse = await fetch(API_URL);
+    let response = await dataresponse.json();
+    console.log("response", response);
+    createCards(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+fetchApi();
+function createCards(data) {
+  data.forEach((user) => {
+    console.log(user);
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    h2.textContent = user.name;
+    let h3 = document.createElement("h3");
+    h3.textContent = user.email;
+    let p = document.createElement("p");
+    p.textContent = user.address.street + " , "  + user.address.city;
+    div.append(h2, h3, p);
+    Card.append(div);
+  });
+}
